55a1e9d66e14689369977e905557e9b53d5080a8
IfIdieTomorrow <chuck46@naver.com>
2021-01-03 21:22:11 +0900

    커스텀 훅을 이용한 뉴스페이지 업그레이드

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
 import { NEWS_URL, KEY, CATEGORY } from '../utils/api';
+import userPromise from '../lib/userPromise';
 
 const NewsListBox = styled.div`
   box-sizing: border-box;
@@ -18,37 +19,28 @@ const NewsListBox = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `${CATEGORY}${category}`;
-        console.log(query);
-        const response = await axios.get(NEWS_URL + KEY + query);
-        setArticles(response.data.articles);
-      } catch (error) {
-        console.log(error);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = userPromise(() => {
+    const query = category === 'all' ? '' : `${CATEGORY}${category}`;
+    return axios.get(NEWS_URL + KEY + query);
   }, [category]);
 
   // 대기 중일 때
   if (loading) {
-    return <NewsListBox>대기 중 ....</NewsListBox>;
+    return <NewsListBox>대기 중 ...</NewsListBox>;
   }
 
   // 아직 articles 값이 설정되지 않았을 때
-  if (!articles) {
+  if (!response) {
     return null;
   }
 
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBox>에러 발생!</NewsListBox>;
+  }
+
   // articles 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBox>
       {articles.map((article) => (
